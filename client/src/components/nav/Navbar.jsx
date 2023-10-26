b70db62b7749ffbff70794c2ddf22b25cbb22472
Abdur Rehman Khalid <akhalid@sandsmedia.com>
2023-10-26 18:44:50 +0200

    Completed the Basic View of Cards

--- client/src/components/nav/Navbar.jsx
+++ client/src/components/nav/Navbar.jsx
@@ -1,9 +1,26 @@
 import './navbar.css';
+import Notification from '../../img/notification.svg';
+import Message from '../../img/message.svg';
+import Settings from '../../img/settings.svg';
 
 function Navbar() {
     return(
         <div className="navbar">
-            navigation bar.
+            <span className='log'>Lama Logo</span>
+            <div className="icons">
+                <div className="icon">
+                    <img src={Notification} className='iconImg' alt='Logo Image' />
+                    <div className="counter">2</div>
+                </div>
+                <div className="icon">
+                    <img src={Message} className='iconImg' alt='Message Icon' />
+                    <div className="counter">2</div>
+                </div>
+                <div className="icon">
+                    <img src={Settings} className='iconImg' alt='Settings Icon' />
+                    <div className="counter">2</div>
+                </div>
+            </div>
         </div>
     )
 }
